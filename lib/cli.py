f8b72274e899d993c876881b0ad1ad780fa32bf8
Victor Hugo <delcarpio.vh@gmail.com>
2024-09-11 01:32:34 -0400

    feat: playlist and song menu

--- lib/cli.py
+++ lib/cli.py
@@ -1,5 +1,6 @@
 from simple_term_menu import TerminalMenu
 from models.playlist import Playlist
+from models.song import Song
 
 def create_playlist():
     name = input("Enter playlist name: ").strip()
@@ -8,6 +9,21 @@ def create_playlist():
 def list_playlists():
     menu_items = ["Back"] + [playlist.name for playlist in Playlist.get_all()]
     terminal_menu = TerminalMenu(menu_items, title="Select a Playlist")
+    while (choice := terminal_menu.show()) != 0:
+        playlist = Playlist.find_by_name(menu_items[choice])
+        playlist_menu(playlist)
+
+def playlist_menu(playlist: Playlist):
+    menu_items = ["Back", "Edit Playlist name", "Delete Playlist", "Add Song to Playlist"] + [song.title for song in playlist.songs()]
+    terminal_menu = TerminalMenu(menu_items, title = f"Select a Song from {playlist.name}")
+    while (choice := terminal_menu.show()) != 0:
+        if choice >= 4: 
+            song = Song.find_by_title(menu_items[choice])
+            song_menu(song)
+
+def song_menu(song: Song):
+    menu_items = ["Back", "Edit Song name", "Edit Artist name", "Delete Song from Playlist"]
+    terminal_menu = TerminalMenu(menu_items, title = f"{song.title}")
     while (choice := terminal_menu.show()) != 0:
         pass
 
