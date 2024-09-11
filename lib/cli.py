bf7742e36e8212b74153d84715f955844ded511f
Victor Hugo <delcarpio.vh@gmail.com>
2024-09-11 01:02:24 -0400

    feat: advanced while loop

--- lib/cli.py
+++ lib/cli.py
@@ -1,41 +1,26 @@
 from simple_term_menu import TerminalMenu
 
+def create_playlist():
+    pass
+
+def list_playlists():
+    pass
 
 def main():
-    options = [
+    functions: dict = {
+        0: create_playlist,
+        1: list_playlists,
+    }
+    
+    options: list = [
         "Create Playlist",
         "List All Playlists",
-        "Edit Playlist Name",
-        "Delete Playlist",
-        "Add Song to Playlist",
-        "Delete Song from Playlist",
         "Exit"
     ]
     
-    while True:
-        terminal_menu = TerminalMenu(options, title="Main Menu")
-        choice_index = terminal_menu.show()
-
-        if choice_index == 0:
-            # create_playlist()
-            pass
-        elif choice_index == 1:
-            # list_playlists()
-            pass
-        elif choice_index == 2:
-            # edit_playlist_name()
-            pass
-        elif choice_index == 3:
-            # delete_playlist()
-            pass
-        elif choice_index == 4:
-            # add_song_to_playlist()
-            pass
-        elif choice_index == 5:
-            # delete_song_from_playlist()
-            pass
-        elif choice_index == 6:
-            break
+    terminal_menu = TerminalMenu(options, title = "Main Menu")
+    while (choice := terminal_menu.show()) != len(options) - 1:
+        functions[choice]()
 
 if __name__ == '__main__':
     main()
