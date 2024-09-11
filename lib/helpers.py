654cc6cfc64cf535f4d9587c80dbf32306f7951f
Victor Hugo <delcarpio.vh@gmail.com>
2024-09-11 14:32:12 -0400

    fix: change and constant variable

--- lib/helpers.py
+++ lib/helpers.py
@@ -3,9 +3,6 @@ from models.playlist import Playlist
 from models.song import Song
 from simple_term_menu import TerminalMenu
 
-CHANGE = 1
-CONSTANT = 0
-
 def create_playlist():
     name = input("Enter playlist name: ").strip()
     Playlist.create(name)
@@ -24,20 +21,20 @@ def playlist_menu(playlist: Playlist):
     while (choice := terminal_menu.show()) != 0:
         if choice == 1:
             edit_playlist_name(playlist)
-            return CHANGE
+            return True
         
         elif choice == 2:
             return delete_playlist(playlist)
         
         elif choice == 3:
             add_song_to_playlist(playlist)
-            return CHANGE
+            return True
             
         if choice >= 4: 
             song = Song.find_by_title(menu_items[choice])
             return song_menu(song)
     
-    return CONSTANT
+    return False
 
 def edit_playlist_name(playlist: Playlist):
     name = input("Enter playlist name: ").strip()
@@ -49,9 +46,9 @@ def delete_playlist(playlist: Playlist):
     terminal_menu = TerminalMenu(menu_items, title = f"Delete {playlist.name}?")
     if terminal_menu.show() == 0:
         playlist.delete()
-        return CHANGE
+        return True
     
-    return CONSTANT
+    return False
 
 def add_song_to_playlist(playlist: Playlist):
     title = input(f"Enter song title for playlist '{playlist.name}': ").strip()
@@ -64,16 +61,16 @@ def song_menu(song: Song):
     if (choice := terminal_menu.show()) != 0:
         if choice == 1:
             edit_song_title(song)
-            return CHANGE
+            return True
         
         elif choice == 2:
             edit_song_artist(song)
-            return CHANGE
+            return True
         
         elif choice == 3:
             return delete_song(song)
     
-    return CONSTANT
+    return False
 
 def edit_song_title(song: Song):
     title = input("Enter song title: ").strip()
@@ -90,6 +87,6 @@ def delete_song(song: Song):
     terminal_menu = TerminalMenu(menu_items, title = f"Delete {song.title}?")
     if terminal_menu.show() == 0:
         song.delete()
-        return CHANGE
+        return True
     
-    return CONSTANT
+    return False
